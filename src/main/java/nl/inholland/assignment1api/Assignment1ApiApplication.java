000512a119bede59e0c61167bace0d40e36d8e58
tsaglisamuel <schaltzchagly@gmail.com>
2021-05-04 00:15:24 +0200

    update

--- src/main/java/nl/inholland/assignment1api/Assignment1ApiApplication.java
+++ src/main/java/nl/inholland/assignment1api/Assignment1ApiApplication.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Bean;
 import javax.xml.bind.SchemaOutputResolver;
 import java.util.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Assignment1ApiApplication {
@@ -27,13 +30,19 @@ public class Assignment1ApiApplication {
 		System.out.println(calculate(10,multiply));
 
 	}
+
+
 	public static Integer calculate(int a,Function<Integer, Integer> m){
 		return m.apply(a);
 	}
+
+
 	public void StringToLength(){
 		Function<String, Integer> stringToLength = a -> a.length();
 		System.out.println(stringToLength.apply("Only when the cotton is high")); //counts the length of string
 	}
+
+
 	public void StringToAllCaps(){
 		Function<String, String> toAllCaps = a -> a.toUpperCase(Locale.ROOT);
 		System.out.println(toAllCaps.apply("Only when the cotton is high")); //converts string to uppercase
@@ -41,6 +50,8 @@ public class Assignment1ApiApplication {
 		UnaryOperator<String>toCaps = a -> a.toUpperCase(Locale.ROOT);
 		System.out.println(toCaps.apply("Only when the cotton is high"));//converts string to uppercase
 	}
+
+
 	public void PredicatesBiFunctionBinaryConsumerSupplier(GamesService service){ //for predicates //bifunction //consumer //supplier
 		Predicate<String> p = a -> a.startsWith("A");
 		System.out.println(p.test("Alphabet")); //returns true
@@ -71,6 +82,57 @@ public class Assignment1ApiApplication {
 	}
 	public static void printString(String s){
 		System.out.println(s);
+	}
+
+	@Bean
+	public CommandLineRunner runner(GamesService service){
+		return args -> {
+			Stream<Integer> stream1 = Stream.empty(); //empty stream
+
+			Stream<Integer> stream2 = Stream.of(1,2,3); //stream of integers //finite ending
+
+			Stream<Double> stream3 = Stream.generate(Math::random); //stream of infinite doubles
+
+			Stream<Integer> stream4 = Stream.iterate(1,i -> i + 1); //generates an infinite stream of integers starting from 1
+			stream4.forEach(System.out::println); //prints integers
+
+			List<String> l= Arrays.asList("a","b","c");
+//			Stream<String> s = list.stream();
+			l.forEach(System.out::println); //prints out the list
+
+ 			//terminal operation
+			List<String>list = List.of("1","2","3");
+			Optional<String> stringOptional = list.stream().filter(a -> a.equals("2")).findAny();
+			System.out.println(stringOptional);
+
+			Stream<Double> list1 = Stream.generate(Math::random);
+			list1.forEach(System.out::println); //prints out generated random doubles
+
+
+			List<String> list2 = List.of("Automobile","batcave","Car","Asparagus");
+			List<String> listA = list2.stream()
+					.filter(a -> a.startsWith("A"))
+					.collect(Collectors.toList());
+			System.out.println(listA); ///prints out the letter in the string beginning with the letter A
+									//OR
+			List<Integer> listIngeters = list2.stream()
+					.map(String::length)
+					.collect(Collectors.toList()); //prnts out the length of the lettters in the list
+			System.out.println(listIngeters);
+
+
+			IntStream.rangeClosed(1,10)
+					.forEach(System.out::println); //prints out 1 to 10
+
+			System.out.println(IntStream.rangeClosed(1,10) //10 elements
+					.filter(i -> i/2==0)
+					.map(i-> i * i) //square root
+					.sum()); //prints out the total sum of even numbers to the power of 2
+
+
+
+
+		};
 	}
 
 		//different ways to do sorting based on highest price (1)
